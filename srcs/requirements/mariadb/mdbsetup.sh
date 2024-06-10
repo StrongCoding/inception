359d1f569dfde8a8e22359450180dac6df976662
dnebatz <dnebatz@student.42wolfsburg.de>
2024-06-10 20:34:21 +0200

    fixed nginx tls config

--- srcs/requirements/mariadb/mdbsetup.sh
+++ srcs/requirements/mariadb/mdbsetup.sh
@@ -9,13 +9,18 @@ chown -R mysql:mysql /run/mysqld
 
 
 echo "hello"
-echo "$MYSQL_DATABASE"
-mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
-mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-mysql -e "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+echo $( cat /run/secrets/db_password)
+echo "bye"
+#echo "$MYSQL_DATABASE"
+#mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
+#mysql -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+#mysql -e "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mysql -e "CREATE DATABASE IF NOT EXISTS $( cat /run/secrets/db_name);"
+mysql -e "CREATE USER IF NOT EXISTS '$( cat /run/secrets/db_user)'@'%' IDENTIFIED BY '$( cat /run/secrets/db_password)';"
+mysql -e "GRANT ALL ON $( cat /run/secrets/db_name).* TO '$( cat /run/secrets/db_user)'@'%' IDENTIFIED BY '$( cat /run/secrets/db_password)';"
 mysql -e "FLUSH PRIVILEGES;"
 
 /etc/init.d/mariadb stop
 
-# Execute the command passed as arguments to the script
+#Execute the command passed as arguments to the script
 exec "$@"
