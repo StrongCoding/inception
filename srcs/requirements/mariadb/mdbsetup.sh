b8c22e1e54174ad1078f6a72853fd2cafa86a66e
dnebatz <dnebatz@student.42wolfsburg.de>
2024-06-17 12:45:18 +0200

    mariadb alpine now running

--- srcs/requirements/mariadb/mdbsetup.sh
+++ srcs/requirements/mariadb/mdbsetup.sh
@@ -9,18 +9,15 @@ chown -R mysql:mysql /run/mysqld
 
 mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 
-echo "hello"
-echo $( cat /run/secrets/db_password)
-echo "bye"
-#echo "$MYSQL_DATABASE"
-#echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
-#echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-#echo "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
+echo "USE mysql;" >> init.sql
+echo "FLUSH PRIVILEGES;" >> init.sql
 echo "CREATE DATABASE IF NOT EXISTS $( cat /run/secrets/db_name);" >> init.sql
-echo "CREATE USER IF NOT EXISTS '$( cat /run/secrets/db_user)'@'%' IDENTIFIED BY '$( cat /run/secrets/db_password)';" >> init.sql
+echo "CREATE USER '$( cat /run/secrets/db_user)'@'%' IDENTIFIED BY '$( cat /run/secrets/db_password)';" >> init.sql
 echo "GRANT ALL ON $( cat /run/secrets/db_name).* TO '$( cat /run/secrets/db_user)'@'%' IDENTIFIED BY '$( cat /run/secrets/db_password)';" >> init.sql
 echo "FLUSH PRIVILEGES;" >> init.sql
 
+
 mysqld --user=mysql --bootstrap < init.sql
 #Execute the command passed as arguments to the script
+rm init.sql
 exec "$@"
