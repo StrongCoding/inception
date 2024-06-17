202828abd5fd262bac78031243c19febe01c1e95
dnebatz <dnebatz@student.42wolfsburg.de>
2024-06-17 14:06:58 +0200

    fixed wpphp alpine

--- srcs/requirements/mariadb/mdbsetup.sh
+++ srcs/requirements/mariadb/mdbsetup.sh
@@ -12,7 +12,7 @@ mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 echo "USE mysql;" >> init.sql
 echo "FLUSH PRIVILEGES;" >> init.sql
 echo "CREATE DATABASE IF NOT EXISTS $( cat /run/secrets/db_name);" >> init.sql
-echo "CREATE USER '$( cat /run/secrets/db_user)'@'%' IDENTIFIED BY '$( cat /run/secrets/db_password)';" >> init.sql
+echo "CREATE USER IF NOT EXISTS '$( cat /run/secrets/db_user)'@'%' IDENTIFIED BY '$( cat /run/secrets/db_password)';" >> init.sql
 echo "GRANT ALL ON $( cat /run/secrets/db_name).* TO '$( cat /run/secrets/db_user)'@'%' IDENTIFIED BY '$( cat /run/secrets/db_password)';" >> init.sql
 echo "FLUSH PRIVILEGES;" >> init.sql
 
