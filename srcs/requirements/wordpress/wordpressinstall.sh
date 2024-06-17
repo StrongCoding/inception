aa9f5bbfabd590f289e9c19730084414cf392d7b
dnebatz <dnebatz@student.42wolfsburg.de>
2024-06-17 12:43:09 +0200

    fixed unused stuff

--- srcs/requirements/wordpress/wordpressinstall.sh
+++ srcs/requirements/wordpress/wordpressinstall.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 cd /var/www/html
 
-#php-fpm7.4 -D
-sleep 11
+sleep 2
 
 
 if [ -f ./wp-config.php ]
@@ -17,8 +16,7 @@ else
 	wp config create --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$WORDPRESS_DB_HOST --allow-root
 	wp core install --url=$WORDPRESS_URL --title="SteveIstCool" --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --allow-root
 	wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --user_pass=$WORDPRESS_USER_PASSWORD --role=author --porcelain --allow-root
-	wp user create steve steve@steve.com --role=author --porcelain --allow-root
-	wp theme install variations --activate --allow-root
+	# wp theme install variations --activate --allow-root
 fi
 
 # Execute the command passed as arguments to the script
